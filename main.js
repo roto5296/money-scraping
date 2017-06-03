704545aa4a5da7ce24ff0e0700049b811fddff15
Roto <hikun5296@gmail.com>
2017-06-03 16:21:28 +0900

    trivial change

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ var money = require('./index.js');
 var fs = require('fs');
 
 var pass = JSON.parse(fs.readFileSync('password.json'));
-//var hoge = new money.bank.smbc(pass.smbc);
-//pass.aeon.options = {"cookie": "aeon-cookie.json"};
-//var hoge = new money.bank.aeon(pass.aeon);
+//var hoge = new money.bank.smbc(pass.bank.smbc);
+//pass.bank.aeon.options = {"cookie": "aeon-cookie.json"};
+//var hoge = new money.bank.aeon(pass.bank.aeon);
 (async ()=>{
   await hoge.login();
-  var data = await hoge.getDetails(2017, 5);
+  var data = await hoge.getDetails(2017, 4);
   console.log(data);
 })();
