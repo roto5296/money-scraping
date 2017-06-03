0c631944cec2f2906ae13fc63845174e252b7e20
Roto <hikun5296@gmail.com>
2017-06-03 23:47:31 +0900

    add suica

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 var bank = require('./lib/bank.js');
+var e_money = require('./lib/e_money.js');
 
 exports.bank = bank;
+exports.e_money = e_money;
--- main.js
+++ main.js
@@ -1,12 +1,33 @@
 var money = require('./index.js');
 var fs = require('fs');
+var rl = require('readline-sync');
 
 var pass = JSON.parse(fs.readFileSync('password.json'));
-//var hoge = new money.bank.smbc(pass.bank.smbc);
-//pass.bank.aeon.options = {"cookie": "aeon-cookie.json"};
-//var hoge = new money.bank.aeon(pass.bank.aeon);
+if (process.argv[2] === "smbc") {
+  var hoge = new money.bank.smbc(pass.bank.smbc);
+} else if (process.argv[2] === "aeon") {
+  pass.bank.aeon.options = {"cookie": "aeon-cookie.json"};
+  var hoge = new money.bank.aeon(pass.bank.aeon);
+} else if (process.argv[2] === "suica") {
+  pass.e_money.suica.options = {"cookie": "suica-cookie.json"};
+  var hoge = new money.e_money.suica(pass.e_money.suica);
+} else {
+  process.exit();
+}
 (async ()=>{
-  await hoge.login();
-  var data = await hoge.getDetails(2017, 4);
+  var flag = 1;
+  var imageString = null;
+  var form = null;
+  while (flag) {
+    var ret = await hoge.login(imageString, form);
+    if (ret) {
+      fs.writeFileSync('/home/shared/tmp.jpg', ret.img, 'binary');
+      imageString = rl.question("input image string ");
+      form = ret.form;
+    } else {
+      flag = 0;
+    }
+  }
+  var data = await hoge.getDetails(2017, 5);
   console.log(data);
 })();
