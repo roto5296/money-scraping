4e594a8f75a2dac42d1840b59fdcd60994dc43a5
Roto <hikun5296@gmail.com>
2018-03-29 21:44:21 +0900

    onetime password for bank-ufj

--- main.js
+++ main.js
@@ -4,57 +4,61 @@ var rl = require('readline-sync');
 var commander = require('commander');
 var googleAuth = require('google-auth-library');
 
-commander.option('-p, --password [value]', 'password JSON').parse(process.argv);
-var pass = JSON.parse(fs.readFileSync(commander.password || 'password.json'));
-var auth = new googleAuth.OAuth2Client(
-  pass.googleapi.web.client_id,
-  pass.googleapi.web.client_secret,
-  pass.googleapi.web.redirect_uris[0]);
-auth.credentials = JSON.parse(fs.readFileSync(process.env.HOME+"/.google_token.json"));
-var list = ['bank-smbc', 'bank-aeon', 'e_money-suica', 'credit-smbc', 'credit-view', 'credit-rakuten', 'credit-pocket']
-var type = commander.args[0];
-if (!type) {
-  type = list[rl.keyInSelect(list, 'Which type? ')];
-}
-switch (type) {
-case "bank-smbc":
-  var hoge = new money.bank.smbc(pass.bank.smbc);
-  break;
-case "bank-aeon":
-  pass.bank.aeon.options.cookie_auth = auth;
-  var hoge = new money.bank.aeon(pass.bank.aeon);
-  break;
-case "bank-ufj":
-  var hoge = new money.bank.ufj(pass.bank.ufj);
-  break;
-case "e_money-suica":
-  pass.e_money.suica.options.cookie_auth = auth;
-  var hoge = new money.e_money.suica(pass.e_money.suica);
-  break;
-case"credit-smbc":
-  var hoge = new money.credit.smbc(pass.credit.smbc);
-  break;
-case "credit-view":
-  var hoge = new money.credit.view(pass.credit.view);
-  break;
-case "credit-rakuten":
-  var hoge = new money.credit.rakuten(pass.credit.rakuten);
-  break;
-case "credit-pocket":
-  var hoge = new money.credit.pocket(pass.credit.pocket);
-  break;
-default:
-  process.exit();
-}
-var year = parseInt(commander.args[1], 10)
-var month = parseInt(commander.args[2], 10)
-if (isNaN(year)) {
-  year = rl.questionInt("year: ")
-}
-if (isNaN(month)) {
-  month = rl.questionInt("month: ")
-}
+
 (async ()=>{
+  commander.option('-p, --password [value]', 'password JSON').parse(process.argv);
+  var pass = JSON.parse(fs.readFileSync(commander.password || 'password.json'));
+  var auth = new googleAuth.OAuth2Client(
+    pass.googleapi.web.client_id,
+    pass.googleapi.web.client_secret,
+    pass.googleapi.web.redirect_uris[0]);
+  auth.credentials.refresh_token = pass.googleapi.refresh_token;
+  await auth.refreshAccessToken();
+  var list = ['bank-smbc', 'bank-aeon', 'e_money-suica', 'credit-smbc', 'credit-view', 'credit-rakuten', 'credit-pocket']
+  var type = commander.args[0];
+  if (!type) {
+    type = list[rl.keyInSelect(list, 'Which type? ')];
+  }
+  switch (type) {
+  case "bank-smbc":
+    var hoge = new money.bank.smbc(pass.bank.smbc);
+    break;
+  case "bank-aeon":
+    pass.bank.aeon.options.cookie_auth = auth;
+    var hoge = new money.bank.aeon(pass.bank.aeon);
+    break;
+  case "bank-ufj":
+    pass.bank.ufj.options = {};
+    pass.bank.ufj.options.gmail_auth = auth;
+    var hoge = new money.bank.ufj(pass.bank.ufj);
+    break;
+  case "e_money-suica":
+    pass.e_money.suica.options.cookie_auth = auth;
+    var hoge = new money.e_money.suica(pass.e_money.suica);
+    break;
+  case"credit-smbc":
+    var hoge = new money.credit.smbc(pass.credit.smbc);
+    break;
+  case "credit-view":
+    var hoge = new money.credit.view(pass.credit.view);
+    break;
+  case "credit-rakuten":
+  var hoge = new money.credit.rakuten(pass.credit.rakuten);
+    break;
+  case "credit-pocket":
+    var hoge = new money.credit.pocket(pass.credit.pocket);
+    break;
+  default:
+    process.exit();
+  }
+  var year = parseInt(commander.args[1], 10)
+  var month = parseInt(commander.args[2], 10)
+  if (isNaN(year)) {
+    year = rl.questionInt("year: ")
+  }
+  if (isNaN(month)) {
+    month = rl.questionInt("month: ")
+  }
   await hoge.waitInit();
   var flag = 1;
   var imageString = null;
